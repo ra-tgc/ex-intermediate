d1c2ec87c98406de6897e34a4b42a02b82b7a612
ra-tgc <masaki.taguchi@rakus.co.jp>
2021-05-26 17:22:41 +0900

    演習2:Hotelサービスを作成

---
+++ src/main/java/com/example/service/HotelService.java
@@ -0,0 +1,37 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.Hotel;
+import com.example.repository.HotelRepository;
+
+/**
+ * ホテル情報を操作するサービス.
+ * 
+ * @author masaki.taguchi
+ *
+ */
+@Service
+@Transactional
+public class HotelService {
+	@Autowired
+	private HotelRepository repository;
+
+	/**
+	 * 値段以下のホテル情報を取得する.
+	 * 
+	 * @param price 値段
+	 * @return 値段以下のホテル情報(値段が存在しない場合は全件取得)
+	 */
+	public List<Hotel> showList(Integer price) {
+		if (price == null) {
+			return repository.findAll();
+		}
+
+		return repository.searchByLessThanPrice(price);
+	}
+}
